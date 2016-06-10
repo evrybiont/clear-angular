193c5dcf818cde470aeaaa575b2410a56da749c8
evrybiont <evrybiont@gmail.com>
2016-06-11 02:23:20 +0300

    asa

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-/*var express = require("express");*/
-/*var path = require("path");*/
-/*var bodyParser = require("body-parser");*/
-/*var app = express();*/
-/*app.use(express.static(__dirname + "/app"));*/
-/*app.use(bodyParser.json());*/
-var express = require('express')
-var app = express()
- 
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+var app = express();
+app.use(express.static(__dirname + "/app"));
+app.use(bodyParser.json());
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
  
-app.listen(3000)
+/*app.listen(3000)*/
