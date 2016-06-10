937d8052ba4f528b27f98512bf8f2a69c19f1b11
evrybiont <evrybiont@gmail.com>
2016-06-11 02:15:16 +0300

    11

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
-var express = require("express");
-var path = require("path");
-var bodyParser = require("body-parser");
-var app = express();
-app.use(express.static(__dirname + "/app"));
-app.use(bodyParser.json());
+/*var express = require("express");*/
+/*var path = require("path");*/
+/*var bodyParser = require("body-parser");*/
+/*var app = express();*/
+/*app.use(express.static(__dirname + "/app"));*/
+/*app.use(bodyParser.json());*/
+var express = require('express')
+var app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.listen(3000)
