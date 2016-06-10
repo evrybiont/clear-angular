ae326e25aa7d02ac90ee5f5d2d3b27cc04689a23
evrybiont <evrybiont@gmail.com>
2016-06-11 01:15:20 +0300

    remove

--- server.js
+++ server.js
@@ -1,17 +1,6 @@
-/*var gzippo = require('gzippo');*/
-/*var express = require('express');*/
-/*var morgan = require('morgan');*/
-/*var app = express();*/
- 
-/*app.use(morgan('combined'));*/
-/*app.use(gzippo.staticGzip("" + __dirname + "/dist"));*/
-/*app.listen(process.env.PORT || 5000);*/
-
 var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
-/*var mongodb = require("mongodb");*/
-/*var ObjectID = mongodb.ObjectID;*/
 
 var CONTACTS_COLLECTION = "contacts";
 
