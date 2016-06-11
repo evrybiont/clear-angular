667c2c1759707a0c8f2763e2f9dc03623da34686
evrybiont <evrybiont@gmail.com>
2016-06-11 15:33:14 +0300

    Setup

--- app/app.js
+++ app/app.js
@@ -1,21 +1,58 @@
 'use strict';
 
 angular.module('frontApp', [
-  'ngRoute'
+  'ngRoute',
+  'route-segment',
+  'view-segment'
 ]).
-  config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  config(['$locationProvider', '$routeProvider', '$routeSegmentProvider', function($locationProvider, $routeProvider, $routeSegmentProvider) {
     /*$locationProvider.hashPrefix('!');*/
 
-  $routeProvider
-    .when('/', {
-      templateUrl: 'views/main.html',
-      controller: 'MainCtrl'
-    })
-    .when('/landpage', {
-      templateUrl: 'views/landpage.html',
-      controller: 'LandPageCtrl'
-    })
-    .otherwise({
-      redirectTo: '/'
-    });
+    $routeSegmentProvider
+      .when('/', 'welcome')
+      .when('/about', 'welcome.about')
+      .when('/create-advert', 'welcome.create_advert')
+      .when('/create-page', 'welcome.create_page')
+      .when('/careers', 'welcome.careers')
+      .when('/developers', 'welcome.developers')
+      .when('/jobs', 'welcome.jobs')
+      .when('/education', 'welcome.education')
+      .when('/freelance', 'welcome.freelance')
+      .when('/privacy', 'welcome.privacy')
+      .segment('welcome', {
+        templateUrl: 'views/layouts/welcome.html'
+      })
+      .within()
+      .segment('landpage', {
+        default: true,
+        templateUrl: 'views/welcome/landpage.html'
+      })
+      .segment('about', {
+        templateUrl: 'views/welcome/about.html'
+      })
+      .segment('create_advert', {
+        templateUrl: 'views/welcome/create_advert.html'
+      })
+      .segment('create_page', {
+        templateUrl: 'views/welcome/create_page.html'
+      })
+      .segment('careers', {
+        templateUrl: 'views/welcome/careers.html'
+      })
+      .segment('developers', {
+        templateUrl: 'views/welcome/developers.html'
+      })
+      .segment('jobs', {
+        templateUrl: 'views/welcome/jobs.html'
+      })
+      .segment('education', {
+        templateUrl: 'views/welcome/education.html'
+      })
+      .segment('freelance', {
+        templateUrl: 'views/welcome/freelance.html'
+      })
+      .segment('privacy', {
+        templateUrl: 'views/welcome/privacy.html'
+      })
+      .up()
 }]);
